cd803e7ed544bda765ecd5d0b2a29440f8b64bee
Matthew Hutchinson <maahutch@stanford.edu>
2022-02-03 12:53:23 -0800

    re-writing entire fuinction so it works

--- count5parse.py
+++ count5parse.py
@@ -1,148 +1,91 @@
-import json
 import pandas as pd
-from itertools import zip_longest
+import json
 
-def counter5parse(input, output): 
+def counter5parse(input):     
     """
-    Reads the Counter5 JSON file, selects the fields desctibed below by the 'cols' list
-    and writes them as a csv file. 
+    Reads the Counter5 JSON file and selects the following fields:
+
+    Platform, 
+    Title, 
+    Publisher, 
+    DataType, 
+    Begin_Date, 
+    End_Date,
+    Total_Item_Investigations,
+    Unique_Item_Requests,
+    Unique_Item_Investigations,
+    Total_Item_Requests,
+    DOI	Proprietary,
+    Online_ISSN,
+    Print_ISSN,
+    No_License
+
+    The function returns a denormalized pandas dataframe.  
 
     :param input: a string path to the raw JSON file
-    :param output: a string path to the CSV file to be created
     """
-
 
     f = open(input)
 
     count5 = json.load(f)
 
-    cols = ['platform', 
-            'begin_date', 
-            'end_date',
-            'instance_total_item_investigations',
-            'instance_unique_item_requests', 
-            'instance_unique_item_investigations',
-            'instance_total_item_requests',
-            'instance_no_license',
-            'item_id_doi',
-            'item_id_proprietary',
-            'item_id_online_ISSN',
-            'item_id_print_ISSN',
-            'title',
-            'publisher_id_proprietary',
-            'publisher',
-            'data_type' ]
+    df = pd.json_normalize(count5)
 
-    d = {}
+    x = pd.DataFrame(df).T.apply(pd.Series.explode).rename_axis('Title').reset_index()
 
-    for i in cols: 
-        d[i] = [i]
+    x = x[x['Title'] == 'Report_Items']
 
-    for i in range(0, len(count5['Report_Items'])):
+    x = x.rename(columns={0:'data'})
 
-        #Platform
-        d['platform'].append(count5['Report_Items'][i]['Platform'])
+
+    x = pd.DataFrame(x['data'].tolist())
+
+
+    counter5 = pd.DataFrame()
+
+    for index, row in x.iterrows():
+
+        oneRow = dict(row)
+
+        output = [{'Platform': oneRow['Platform'],
+                    'Title':   oneRow['Title'],
+                    'Publisher': oneRow['Publisher'],
+                    'DataType': oneRow['Data_Type']
+                    }]
+
+        outDF = pd.DataFrame(output)
+
+        date = pd.DataFrame()
+        metricDF = pd.DataFrame()
+
+        for i in range(0, len(oneRow['Performance'])):
+            date = date.append(pd.DataFrame(pd.Series(oneRow['Performance'][0]['Period'])).T)
+
+            metric = oneRow['Performance'][i]['Instance']
+            metricMod = dict(d.values() for d in metric)
+            oneMetricDF  = pd.DataFrame(metricMod, index = [0])
+            metricDF = metricDF.append(oneMetricDF)
+
+
+        date = pd.concat([date.reset_index(drop=True),
+                            metricDF.reset_index(drop=True)], axis=1)
         
-        for j in range(0, len(count5['Report_Items'][i]['Performance'])):
-
-        #begin_date
-            try:     
-                d['begin_date'].append(count5['Report_Items'][i]['Performance'][j]['Period']['Begin_Date'])
-            except: 
-                d['begin_date'].append('NA')
-
-        #end_date
-            try: 
-                d['end_date'].append(count5['Report_Items'][i]['Performance'][j]['Period']['End_Date'])
-            except:
-                d['end_date'].append('NA')
+        outDF = outDF.merge(date, how='cross')
 
 
-            instSub = count5['Report_Items'][i]['Performance'][j]['Instance']
-
-            for l in range(0, len(instSub)):
-
-                    #instance_total_item_investigations
-                    if instSub[l]['Metric_Type'] == 'Total_Item_Investigations':
-                        try:
-                            d['instance_total_item_investigations'].append(instSub[l]['Count'])
-                        except: 
-                            d['instance_total_item_investigations'].append('NA')
-
-                    #instance_unique_item_requests
-                    elif  instSub[l]['Metric_Type'] == 'Unique_Item_Requests': 
-                        try:
-                            d['instance_unique_item_investigations'].append(instSub[l]['Count'])
-                        except: 
-                            d['instance_unique_item_investigations'].append('NA')
-                    
-                    #instance_unique_item_investigations
-                    elif  instSub[l]['Metric_Type'] == 'Unique_Item_Investigations':
-                        try:
-                            d['instance_unique_item_requests'].append(instSub[l]['Count'])
-                        except: 
-                            d['instance_unique_item_requests'].append('NA')
-
-                    #instance_total_item_requests
-                    elif instSub[l]['Metric_Type'] == 'Total_Item_Requests':
-                        try: 
-                            d['instance_total_item_requests'].append(instSub[l]['Count'])
-                        except: 
-                            d['instance_total_item_requests'].append('NA')
-                    
-                    #instance_no_license
-                    elif  instSub[l]['Metric_Type'] == 'No_License':
-                        try: 
-                            d['instance_no_license'].append(instSub[l]['Count'])
-                        except: 
-                            d['instance_no_license'].append('NA')
-
-        #DOI
-            try: 
-                d['item_id_doi'].append(count5['Report_Items'][i]['Item_ID'][0]['Value'])
-            except: 
-                d['item_id_doi'].append('NA')
-        #Proprietary
-            try:
-                d['item_id_proprietary'].append(count5['Report_Items'][i]['Item_ID'][1]['Value'])
-            except: 
-                d['item_id_proprietary'].append('NA') 
-        #Online_ISSN
-            try:
-                d['item_id_online_ISSN'].append(count5['Report_Items'][i]['Item_ID'][2]['Value'])
-            except: 
-                d['item_id_online_ISSN'].append('NA') 
-        #Print_ISSN
-            try:
-                d['item_id_print_ISSN'].append(count5['Report_Items'][i]['Item_ID'][3]['Value'])
-            except: 
-                d['item_id_print_ISSN'].append('NA') 
-
-        #Title
-            try: 
-                d['title'].append(count5['Report_Items'][i]['Title'])
-            except: 
-                d['title'].append('NA')
+        id = oneRow['Item_ID']
+        idMod = dict(d.values() for d in id)
+        oneIdDF = pd.DataFrame(idMod, index = [0])
         
-        #Publisher ID
-            try: 
-                d['publisher_id_proprietary'].append(count5['Report_Items'][i]['Publisher_ID'][0]['Value'])
-            except: 
-                d['publisher_id_proprietary'].append('NA')
-        
-        #Publisher
-            try:
-                d['publisher'].append(count5['Report_Items'][i]['Publisher'])
-            except:
-                d['publisher'].append('NA')
-        
-        #Data_Type
-            try:
-                d['data_type'].append(count5['Report_Items'][i]['Data_Type'])
-            except:
-                d['data_type'].append('NA')
+        outDF = outDF.merge(oneIdDF, how='cross')
 
-    zl = list(zip_longest(*d.values()))
-    df = pd.DataFrame(zl, columns=d.keys())
+        counter5 = counter5.append(outDF)
+    
+    return(counter5)
+    
+    
 
-    df.to_csv(output, index=False, header =T)
+if __name__ == "__main__":
+    out = counter5parse('rawCat5.json')
+
+    out.to_csv('output.csv', index=False, header = True)
